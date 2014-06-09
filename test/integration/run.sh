3456b684f525ce3c757e9555d251a52f17fb25e6
Johannes Schickling <schickling.j@gmail.com>
2014-06-09 17:28:29 +0200

    Fixed ip injecting

--- test/integration/run.sh
+++ test/integration/run.sh
@@ -1,30 +1,37 @@
 #! /bin/bash
 set -e
 
+PLATFORM=$1
 SCRIPT_DIR=$( cd $(dirname $0) ; pwd -P )
+PLUGIN_DIR="$SCRIPT_DIR/../.."
 PLUGIN_NAME="me.schickling.ibeacon"
 LOCAL_IP=$( ifconfig en0 | grep 'inet ' | cut -d' ' -f2 )
 
-# setup
+# change base dir
 cd $SCRIPT_DIR
+
+# inject local ip for socket server
+cp -f www/index.html www/index.html.org
+cp -f www/js/index.js www/js/index.js.org
+sed -i '' "s/LOCAL_IP/$LOCAL_IP/g" www/index.html
+sed -i '' "s/LOCAL_IP/$LOCAL_IP/g" www/js/index.js
+
+# clean up & link plugin
 rm -rf platforms plugins
 mkdir platforms plugins
-ln -s $SCRIPT_DIR/../.. $SCRIPT_DIR/plugins/$PLUGIN_NAME
+ln -s $PLUGIN_DIR $SCRIPT_DIR/plugins/$PLUGIN_NAME
+
+# add platforms
 cordova platform add android
 cordova platform add ios
-
-# inject socket server ip
-sed -i '' "s/LOCAL_IP/$LOCAL_IP/g" platforms/android/assets/www/index.html
-sed -i '' "s/LOCAL_IP/$LOCAL_IP/g" platforms/android/assets/www/js/index.js
-sed -i '' "s/LOCAL_IP/$LOCAL_IP/g" platforms/ios/www/index.html
-sed -i '' "s/LOCAL_IP/$LOCAL_IP/g" platforms/ios/www/js/index.js
 
 # start socket server
 node server.js &
 
-# run on devices
-cordova run android
-#cordova run ios
+# run on device
+cordova run $PLATFORM
 
-# cleanup
-cd -
+# clean up
+rm www/{index.html,js/index.js}
+mv www/index.html.org www/index.html
+mv www/js/index.js.org www/js/index.js
