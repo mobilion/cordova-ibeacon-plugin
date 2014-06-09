a17bd95ac7306e6e5936b289c92a2be92b0269f4
Johannes Schickling <schickling.j@gmail.com>
2014-06-09 23:31:30 +0200

    ranging tests

--- test/integration/www/spec/02-ranging.js
+++ test/integration/www/spec/02-ranging.js
@@ -7,12 +7,28 @@ describe('ranging', function() {
     uuid: uuid.v4(),
   });
 
-  var tolerance;
-
-  beforeEach(function() {
-    tolerance = 1;
-  });
-
   afterEach(clean);
+
+  it('should range on beacon', function(done) {
+
+    var major = 12345;
+    var minor = 67890;
+
+    advertise(region.uuid, major, minor);
+
+    ibeacon.startRangingBeaconsInRegion(region, function(result) {
+
+      expect(result.beacons.length).toBe(1);
+      expect(result.beacons[0].uuid).toBe(region.uuid);
+      expect(result.beacons[0].major).toBe(major);
+      expect(result.beacons[0].minor).toBe(minor);
+      expect(region.equals(new ibeacon.Region(result.region))).toBe(true);
+      
+      ibeacon.stopRangingBeaconsInRegion(region);
+      done();
+
+    });
+
+  });
 
 });
