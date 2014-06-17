40144071435270d33c124c6e25554176a1cc3891
Johannes Schickling <schickling.j@gmail.com>
2014-06-17 11:54:21 +0200

    added advertising test

--- test/integration/www/spec/01-advertising.js
+++ test/integration/www/spec/01-advertising.js
@@ -2,6 +2,35 @@
 
 describe('advertising', function() {
 
+  var testUuid = uuid.v4();
 
+  var beaconA = new ibeacon.Beacon({
+    uuid: testUuid,
+    major: 11111,
+    minor: 22222,
+  });
+
+  var beaconB = new ibeacon.Beacon({
+    uuid: testUuid,
+    major: 11111,
+    minor: 22223,
+  });
+
+  it('should advertise one beacon', function(done) {
+
+    var options = {
+      beacon: beaconA,
+    };
+
+    ibeacon.startAdvertising(options);
+
+    scan(beaconA.uuid, beaconA.major, beaconA.minor, function(beaconWasFound) {
+      
+      expect(beaconWasFound).toBe(true);
+      done();
+
+    });
+
+  });
 
 });
